2d40fc3a102720412a033fa275e6f7cb437de901
Maz801054 <onemake125@gmail.com>
2023-06-22 21:12:57 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function Netfilx() {
+  var a = document.getElementById("first").value;
+  if (a == "") {
+    alert("Please enter your email ");
+  }
+  var b = document.getElementById("second").value;
+  if (b == "") {
+    alert("Please enter your password");
+  } else {
+    alert("Thanks for submition!");
+  }
+}
